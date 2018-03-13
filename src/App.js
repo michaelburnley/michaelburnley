801f6937dd767750e0c8908bfd062ebc10cd619d
Michael Burnley <michael@cybercowgames.com>
2018-03-13 13:35:29 -0700

    Adding site-block information file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css'; 
+import sb from './site-block-information.json'
 
 class SiteBlock extends Component {
+  handleExpand(){
+
+  }
+
   render() {
     return (
-      
+      <div className="site-block">
+        <h2></h2>
+      </div>
+      <ExpandButton 
+        handleClick={this.handleExpand} />
     );
   }
 }
@@ -18,24 +27,26 @@ class ContactForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-        Name
-        <input type="text" />
-        </label>
-        <label>
-        Email
-        <input type="text" />
-        </label>
-        <label>
-        Subject
-        <input type="text" />
-        </label>
-        <label>
-        Content
-        <textarea type="textarea" />
-        </label>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+          Name
+          <input type="text" />
+          </label>
+          <label>
+          Email
+          <input type="text" />
+          </label>
+          <label>
+          Subject
+          <input type="text" />
+          </label>
+          <label>
+          Content
+          <textarea type="textarea" />
+          </label>
+        </form>
+      </div>
     );
   }
 }
@@ -55,15 +66,17 @@ class ExpandButton extends Component {
 }
 
 
+
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      blocks = sb
     }
   }
 
-  handleSubmit(e) {
+  contactSubmit(e) {
 
   }
 
@@ -74,7 +87,10 @@ class App extends Component {
 
   render() {
     return (
-      <ContactForm />
+      <ContactForm 
+        handleSubmit={this.contactSubmit} />
+      <SiteBlock />
+
     );
   }
 }
